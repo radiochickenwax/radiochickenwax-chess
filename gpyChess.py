2e97e149437cb9aae92a58d5e9710f5cc0ba952a
gregRaspbianN900T <gregorio.jp.mitchell@gmail.com>
2017-10-28 15:35:06 -0700

    added board

--- gpyChess.py
+++ gpyChess.py
@@ -7,8 +7,52 @@ import sys
 
 # piece has an x,y coordinate and a type
 
+class Board:
+    def stringToArray(self):
+        self.splitStrings = self.string.split('\n')
 
-def evaluatePawn():
+    def boardArrayToString(self):
+        self.string = ''
+        for i in range(len(self.splitStrings)):
+            self.string += self.splitStrings[i]+'\n'
+            
+    def __init__(self, string_i = ''):
+        if string_i == '':
+            self.string = "rkbqkbkr\npppppppp\n........\n........\n........\n........\nPPPPPPPP\nRKBQKBKR"
+        else:
+            self.string = string_i
+        self.splitString = self.stringToArray()
+#\npppppppp\n........\n........\n........\n........\nPPPPPPPP\nRKBQKBKR"
+
+
+# self.arr = [
+        #     "rkbqkbkr",
+        #     "pppppppp",
+        #     "........",
+        #     "........",
+        #     "........",
+        #     "........",
+        #     "PPPPPPPP",                       
+        #     "RKBQKBKR"
+        # ]
+
+    # this method doesn't work - using array directly
+    def getPieceAtPoint(self, point):
+        return self.string[point.y*8]
+
+class Point:
+    def __init__(self, xi, yi):
+        self.x = xi
+        self.y = yi
+
+class Piece:
+    def __init__(self, type_i, point_i):
+        self.type = type_i
+        self.point = point_i
+        self.moved = False
+        # self.history = []
+        
+def evaluatePawn(Piece):
     # is starting position?
     return None
 
@@ -27,9 +71,7 @@ def evaluateBishop():
     # return possible moves
     return None
 
-def evaluateQueen():def evaluatePawn():
-    return None
-
+def evaluateQueen():
     return None
 
 def evaluateKing():
