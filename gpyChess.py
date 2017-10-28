162d9f5745895e68111966a2a9d9975b72c901a8
gregRaspbianN900T <gregorio.jp.mitchell@gmail.com>
2017-10-28 10:57:53 -0700

    gpyChess.py

---
+++ gpyChess.py
@@ -0,0 +1,37 @@
+import sys
+
+# pieces are what?
+# an array of type piece
+
+# board
+
+# piece has an x,y coordinate and a type
+
+
+def evaluatePawn():
+    # is starting position?
+    return None
+
+def evaluateRook():
+    return None
+
+def evaluateKnight():
+    return None
+
+def evaluateBishop():
+    # check all diagonals
+    # check left diagonal forward
+    # check right diagonal forward
+    # check left diagonal backward
+    # check right diagonal backward
+    # return possible moves
+    return None
+
+def evaluateQueen():def evaluatePawn():
+    return None
+
+    return None
+
+def evaluateKing():
+    return None
+
